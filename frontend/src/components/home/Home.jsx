7b2dffdb4913ebf3f55ce7ce5928dd0a8d3be371
César Augusto <cesar_augusto_perfil@hotmail.com>
2020-05-17 10:40:13 -0300

    crud-com-react-e-js

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,9 @@
+import Main from '../template/Main';
+import React from 'react';
+
+export default props => 
+    <Main icon="home" title="Início" subtitle="Projeto de ReactJS">
+        <div className="disply-4">Bem-vindo!</div>
+        <hr />
+        <p className="mb-0">Sistema para exemplificar a construção de um cadatro em ReactJS.</p>
+    </Main>
